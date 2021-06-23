52448a03b6dccbc730728d197030d9c619b8ba0f
rainy-me <github@rainy.me>
2021-06-23 13:23:01 +0900

    feat: tweaks

--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,12 @@
-<?php
+<?
+$server = "mysql";
+$username = "root";
+$password = "root";
 
-phpinfo();
+$conn = new mysqli($server, $username, $password);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
